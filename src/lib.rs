05808731f28bdb829757b89a3fd3cf83debb4753
karthik2804 <karthik.ganeshram@fermyon.com>
2024-10-16 16:28:09 +0200

    resize image based on query parameter
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,36 @@
+use photon_rs::transform::resize;
 use spin_sdk::http::{IntoResponse, Request, Response};
 use spin_sdk::http_component;
 
 extern crate photon_rs;
+extern crate url;
+use std::str::FromStr;
+use url::Url;
+
 use photon_rs::filters;
 use photon_rs::native::open_image;
 
 /// A simple Spin HTTP component.
 #[http_component]
-fn handle_photon_image(_req: Request) -> anyhow::Result<impl IntoResponse> {
+fn handle_photon_image(req: Request) -> anyhow::Result<impl IntoResponse> {
+    let url = req.uri();
+    let parsed_url = Url::parse(&url)?;
+
+    // Extract query parameters using `url::Url`
+    let width = parsed_url
+        .query_pairs()
+        .find(|(key, _)| key == "width")
+        .and_then(|(_, value)| u32::from_str(&value).ok());
+    let height = parsed_url
+        .query_pairs()
+        .find(|(key, _)| key == "height")
+        .and_then(|(_, value)| u32::from_str(&value).ok());
+
     let mut img = open_image("image.jpg").expect("File should open");
-    filters::filter(&mut img, "oceanic");
+
+    if let (Some(w), Some(h)) = (width, height) {
+        img = resize(&img, w, h, photon_rs::transform::SamplingFilter::Nearest);
+    }
 
     let bytes = img.get_bytes_jpeg(255);
 
